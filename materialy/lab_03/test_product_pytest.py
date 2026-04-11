c43f8d9f9cb23faab53ce1e03cb30cd65e0b8d13
Piotr Marczak <141434625+icewuuu@users.noreply.github.com>
2026-04-11 15:01:48 +0200

    lab03

--- materialy/lab_03/test_product_pytest.py
+++ materialy/lab_03/test_product_pytest.py
@@ -14,7 +14,7 @@ from product import Product
 def product():
     """Tworzy instancje Product do testow (odpowiednik setUp)."""
     # TODO: Zwroc instancje Product, np. Product("Laptop", 2999.99, 10)
-    pass
+    return Product("Laptop", 100, 10)
 
 
 # --- Testy z fixture ---
@@ -22,13 +22,13 @@ def product():
 def test_is_available(product):
     """Sprawdz dostepnosc produktu."""
     # TODO: Uzyj assert product.is_available() == True
-    pass
+    assert product.is_available() == True
 
 
 def test_total_value(product):
     """Sprawdz wartosc calkowita."""
     # TODO: Uzyj assert product.total_value() == oczekiwana_wartosc
-    pass
+    assert product.total_value() == product.price * product.quantity
 
 
 # --- Testy z parametryzacja ---
@@ -38,22 +38,43 @@ def test_total_value(product):
     # (5, 15),   # dodanie 5 do poczatkowych 10 = 15
     # (0, 10),   # dodanie 0 = bez zmian
     # (100, 110),  # dodanie 100
+    (5, 15),
+    (0, 10),
+    (100, 110),
 ])
 def test_add_stock_parametrized(product, amount, expected_quantity):
     """Testuje add_stock z roznymi wartosciami."""
     # TODO: Wywolaj product.add_stock(amount) i sprawdz product.quantity
-    pass
-
+    product.add_stock(amount)
+    assert product.quantity == expected_quantity
 
 # --- Testy bledow ---
 
 def test_remove_stock_too_much_raises(product):
     """Sprawdz, czy proba usuniecia za duzej ilosci rzuca ValueError."""
     # TODO: Uzyj with pytest.raises(ValueError):
-    pass
+    with pytest.raises(ValueError):
+        product.remove_stock(11)
 
 
 def test_add_stock_negative_raises(product):
     """Sprawdz, czy ujemna wartosc w add_stock rzuca ValueError."""
     # TODO: Uzyj with pytest.raises(ValueError):
-    pass
+    with pytest.raises(ValueError):
+        product.add_stock(-11)
+    
+
+@pytest.mark.parametrize("discount, expected_price", [
+    # TODO: Dodaj przypadki testowe jako krotki, np.:
+    # (5, 15),   # dodanie 5 do poczatkowych 10 = 15
+    # (0, 10),   # dodanie 0 = bez zmian
+    # (100, 110),  # dodanie 100
+        (0, 100.0),
+        (50, 50.0),
+        (100, 0.0),
+    ])
+def test_add_stock_parametrized(product, discount, expected_price):
+    """Testuje add_stock z roznymi wartosciami."""
+    # TODO: Wywolaj product.add_stock(amount) i sprawdz product.quantity
+    product.apply_discount(discount)
+    assert product.price == expected_price
